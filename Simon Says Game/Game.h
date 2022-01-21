61eeb865a3e9683c381ec0a54ed3393de3306f40
(Student) - Krystian Sendyk <C00273292@itcarlow.ie>
2022-01-21 10:29:12 +0000

    added the red button

---
+++ Simon Says Game/Game.h
@@ -0,0 +1,45 @@
+/// <summary>
+/// Name: Krystian Sendyk
+/// Date: 21/01/2022
+/// </summary>
+#ifndef GAME_HPP
+#define GAME_HPP
+/// <summary>
+/// include guards used so we don't process this file twice
+/// same as #pragma once
+/// Don't forget the endif at the bottom
+/// </summary>
+#include <SFML/Graphics.hpp>
+
+class Game
+{
+public:
+	Game();
+	~Game();
+	/// <summary>
+	/// main method for game
+	/// </summary>
+	void run();
+
+private:
+
+	void processEvents();
+	void processKeys(sf::Event t_event);
+	void update(sf::Time t_deltaTime);
+	void render();
+	
+	void m_buttons();
+
+	const sf::Color RED{ 194, 0 , 0 , 0 };
+
+	sf::RectangleShape m_redButton;
+
+	sf::RenderWindow m_window; // main SFML window
+	sf::Font m_ArialBlackfont; // font used by message
+	
+	bool m_exitGame; // control exiting game
+
+};
+
+#endif // !GAME_HPP
+
